94c9353251ed0afbba7d96ec797736f094077650
Josefu Maeda <162345785+JosefuMaeda@users.noreply.github.com>
2026-02-10 15:53:58 +0800

    Add files via upload

---
+++ InterestCalculator/InterestCalculator/InterestCalculator.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+class InterestCalculator
+{
+    static void Main()
+    {
+        Console.Write("Enter principal amount: ");
+        double principal = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Enter rate of interest: ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Enter time in years: ");
+        double time = Convert.ToDouble(Console.ReadLine());
+        
+        double simpleInterest = (principal * rate * time) / 100;
+        
+        Console.WriteLine($"Simple Interest: {simpleInterest:F2}");
+    }
+}
